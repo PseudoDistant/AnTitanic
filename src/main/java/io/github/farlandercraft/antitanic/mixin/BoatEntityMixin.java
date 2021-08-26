6b1d51513fbb83d933e1ba6d0f1001716307fe96
FarlanderCraft <javafarlander2002@gmail.com>
2021-08-25 20:17:57 -0400

    BOTE ONE POINT SEVEN

--- src/main/java/io/github/farlandercraft/antitanic/mixin/BoatEntityMixin.java
+++ src/main/java/io/github/farlandercraft/antitanic/mixin/BoatEntityMixin.java
@@ -1,9 +1,10 @@
 package io.github.farlandercraft.antitanic.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.item.Item;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.GameRuleManager;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,17 +18,17 @@ public class BoatEntityMixin extends Entity {
 	}
 
 	// Make the boat not die when it hits a photon.
-	@Redirect(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;remove()V"))
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;remove()V"))
 	public void BoatInject(BoatEntity boatEntity) {
-
+		System.out.println();
 	}
 	// Make the boat not spawn Wooden Pickaxe recipe when it hits a photon.
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRuleManager;getBoolean(Ljava/lang/String;)Z"))
-	public boolean BoatInject2(GameRuleManager gameRuleManager, String name) {
-		return false;
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;method_6944(Lnet/minecraft/item/Item;IF)Lnet/minecraft/entity/ItemEntity;"))
+	public ItemEntity BoatInject2(BoatEntity boatEntity, Item item, int i, float f) {
+		return null;
 	}
 
-	@Shadow public void initDataTracker() {}
-	@Shadow public void readCustomDataFromTag(CompoundTag tag) {}
-	@Shadow public void writeCustomDataToTag(CompoundTag tag) {}
+	@Shadow	protected void initDataTracker() {}
+	@Shadow protected void readCustomDataFromTag(CompoundTag tag) {}
+	@Shadow protected void writeCustomDataToTag(CompoundTag tag) {}
 }
