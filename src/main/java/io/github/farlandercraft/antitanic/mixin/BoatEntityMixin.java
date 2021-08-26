43090b13e7cbc38e4d960e3f9cb5e585967f288a
FarlanderCraft <javafarlander2002@gmail.com>
2021-08-25 20:28:04 -0400

    BOTE IMPROVEE

--- src/main/java/io/github/farlandercraft/antitanic/mixin/BoatEntityMixin.java
+++ src/main/java/io/github/farlandercraft/antitanic/mixin/BoatEntityMixin.java
@@ -1,9 +1,10 @@
 package io.github.farlandercraft.antitanic.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.item.Item;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.GameRuleManager;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,9 +23,9 @@ public class BoatEntityMixin extends Entity {
 
 	}
 	// Make the boat not spawn Wooden Pickaxe recipe when it hits a photon.
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRuleManager;getBoolean(Ljava/lang/String;)Z"))
-	public boolean BoatInject2(GameRuleManager gameRuleManager, String name) {
-		return false;
+	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;dropItem(Lnet/minecraft/item/Item;IF)Lnet/minecraft/entity/ItemEntity;"))
+	public ItemEntity BoatInject2(BoatEntity boatEntity, Item item, int i, float yOffset) {
+		return null;
 	}
 
 	@Shadow public void initDataTracker() {}
