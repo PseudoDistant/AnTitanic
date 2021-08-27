1159038421cad3540b484027a0f839bee97a2c15
FarlanderCraft <javafarlander2002@gmail.com>
2021-08-27 12:26:24 -0400

    BOTE DO DAMAG DOE

--- src/main/java/io/github/farlandercraft/antitanic/mixin/BoatEntityMixin.java
+++ src/main/java/io/github/farlandercraft/antitanic/mixin/BoatEntityMixin.java
@@ -1,34 +1,93 @@
 package io.github.farlandercraft.antitanic.mixin;
 
+import io.github.farlandercraft.antitanic.AnTitanic;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+import java.util.List;
 
 @Mixin(BoatEntity.class)
-public class BoatEntityMixin extends Entity {
+public abstract class BoatEntityMixin extends Entity {
+	@Shadow protected abstract boolean canClimb();
+
 	public BoatEntityMixin(World world) {
 		super(world);
 	}
-
-	// Make the boat not die when it hits a photon.
-	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;remove()V"))
-	public void BoatInject(BoatEntity boatEntity) {
-		System.out.println();
+	//this.canClimb() = true
+	@Redirect(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;remove()V"))
+	public void PreventBoatDisintegration(BoatEntity boatEntity) {
+		this.velocityX *= 0.9900000095367432D;
+		this.velocityY *= 0.949999988079071D;
+		this.velocityZ *= 0.9900000095367432D;
 	}
-	// Make the boat not spawn Wooden Pickaxe recipe when it hits a photon.
+
+
 	@Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;method_6944(Lnet/minecraft/item/Item;IF)Lnet/minecraft/entity/ItemEntity;"))
-	public ItemEntity BoatInject2(BoatEntity boatEntity, Item item, int i, float f) {
+	public ItemEntity RemoveItemDrops(BoatEntity boatEntity, Item item, int i, float f) {
 		return null;
 	}
 
-	@Shadow	protected void initDataTracker() {}
-	@Shadow protected void readCustomDataFromTag(CompoundTag tag) {}
-	@Shadow protected void writeCustomDataToTag(CompoundTag tag) {}
+	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getOtherEntities(Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Box;)Ljava/util/List;"))
+	public void BoatRammer(CallbackInfo ci) {
+		List list = this.world.getOtherEntities(this, this.boundingBox.expand(0.20000000298023224D,
+				0.0D, 0.20000000298023224D));
+		if (list != null && !list.isEmpty()) {
+			for(int ao = 0; ao < list.size(); ++ao) {
+				Entity entity = (Entity)list.get(ao);
+				if (entity != this.rider && entity.isPushable()) {
+					if (entity instanceof BoatEntity) {
+						entity.pushAwayFrom(this);
+					} else {
+						if (Math.sqrt(Math.pow(this.velocityX, 2) + Math.pow(this.velocityZ, 2)) >= 0.23D && AnTitanic.config.boatsDealDamage) {
+							entity.damage(DamageSource.GENERIC,
+									((float) (Math.sqrt(Math.pow(this.velocityX, 2) + Math.pow(this.velocityZ, 2)) - 0.15D) * 55));
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	/** @author Distant */
+	@Overwrite
+	public void updateFall(double heightDifference, boolean onGround) {
+		int var4 = MathHelper.floor(this.x);
+		int var5 = MathHelper.floor(this.y);
+		int var6 = MathHelper.floor(this.z);
+		if (onGround) {
+			if (this.fallDistance > 3.0F) {
+				if (!this.world.isClient && !this.removed) {
+					this.remove();
+					this.method_6944(Items.BOAT, 1, 0.0F);
+					this.rider.updatePosition(this.x, this.y + 1, this.z);
+				}
+				this.fallDistance = 0.0F;
+			}
+		} else if (this.world.getBlock(var4, var5 - 1, var6).getMaterial() != Material.WATER && heightDifference < 0.0D) {
+			this.fallDistance = (float)((double)this.fallDistance - heightDifference);
+		}
+	}
+
+	@Shadow public void initDataTracker() {}
+	@Shadow public void readCustomDataFromTag(CompoundTag tag) {}
+	@Shadow public void writeCustomDataToTag(CompoundTag tag) {}
 }
