09fbed513cb797bae367c5732fde3b4aaa1efbcd
FarlanderCraft <javafarlander2002@gmail.com>
2021-12-31 16:30:19 -0500

    Partial update to RewovenMC, added speed multiplier option.

--- src/main/java/io/github/farlandercraft/antitanic/AnTitanicConfig.java
+++ src/main/java/io/github/farlandercraft/antitanic/AnTitanicConfig.java
@@ -4,9 +4,11 @@ public class AnTitanicConfig {
     public boolean boatsDealDamage;
     public boolean volatileBoats;
     public boolean iceBreaksBoats;
-    public AnTitanicConfig(boolean bDD, boolean vB, boolean antiantitanic) {
+    public double speedMultiplier;
+    public AnTitanicConfig(boolean bDD, boolean vB, boolean antiantitanic, double boatSpeed) {
         this.boatsDealDamage = bDD;
         this.volatileBoats = vB;
         this.iceBreaksBoats = antiantitanic;
+        this.speedMultiplier = boatSpeed;
     }
 }
